997c125873eb1de7338dda477a988d08534b98dd
JoeVincentt <eugene.bb@hotmail.com>
2019-01-05 13:13:58 -0500

    added styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,14 +13,14 @@ const Navbar = () => (
     shape="roundedBottom"
   >
     {/* Sign In Link */}
-    <NavLink to="/signin">
+    <NavLink activeClassName="active" to="/signin">
       <Text size="xl" color="white">
         Sign In
       </Text>
     </NavLink>
 
     {/* Title and Logo */}
-    <NavLink to="/">
+    <NavLink activeClassName="active" exact to="/">
       <Box display="flex" alignItems="center">
         <Box margin={2} height={50} width={50}>
           <Image
@@ -38,7 +38,7 @@ const Navbar = () => (
     </NavLink>
 
     {/* Sign Up Link */}
-    <NavLink to="/signin">
+    <NavLink activeClassName="active" to="/signup">
       <Text size="xl" color="white">
         Sign Up
       </Text>
