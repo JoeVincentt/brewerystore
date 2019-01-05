48ae706155b903f04e478b05edd93484717e174f
JoeVincentt <eugene.bb@hotmail.com>
2019-01-05 12:49:15 -0500

    created components and router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} exact path="/" />
+      <Route component={Signin} exact path="/signin" />
+      <Route component={Signup} exact path="/signup" />
+      <Route component={Checkout} exact path="/checkout" />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
