e3b601adfcd2fdc45e2f20e7bd5cffd2c891bf2a
JoeVincentt <eugene.bb@hotmail.com>
2019-01-05 14:37:11 -0500

    displaying cards

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
-import { Container, Box, Heading } from "gestalt";
+import { Container, Box, Heading, Card, Image, Text } from "gestalt";
 
 import "./App.css";
 import Strapi from "strapi-sdk-javascript/build/main";
+import { Link } from "react-router-dom";
 
 const apiUrl = process.env.API_URL || " http://localhost:1337";
 const strapi = new Strapi(apiUrl);
@@ -38,6 +39,7 @@ class App extends Component {
   }
 
   render() {
+    const { brands } = this.state;
     return (
       <Container>
         {/* Brands section */}
@@ -46,6 +48,40 @@ class App extends Component {
           <Heading color="midnight" size="md">
             Brew Brands
           </Heading>
+        </Box>
+        {/* Brands */}
+        <Box display="flex" justifyContent="around">
+          {brands.map(brand => (
+            <Box width={200} margin={2} key={brand._id}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.image.url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">
+                    {brand.name}
+                  </Text>
+                  <Text>{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand._id}`}>See Brews</Link>
+                  </Text>
+                </Box>
+              </Card>
+            </Box>
+          ))}
         </Box>
       </Container>
     );
